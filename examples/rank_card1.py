36a2e9db6c9e7b1812f2c9a7d4193564434acc9d
Shahriyar <mdshahriyaralam552@gmail.com>
2021-08-10 20:03:50 +0600

    Added rank_card1 example

---
+++ examples/rank_card1.py
@@ -0,0 +1,36 @@
+from easy_pil import Font, Canvas, Editor
+
+user_data = { # Most likely coming from database or calculation
+    'name': 'Shahriyar#9770', # The user's name
+    'xp': 1240,
+    'next_level_xp': 5000,
+    'level': 5,
+    'percentage': 45,
+}
+
+background = Editor(canvas=Canvas((900, 300), color="#23272A"))
+profile = Editor("assets/pfp.png").resize((150, 150)).circle_image()
+
+# For profile to use users profile picture load it from url using the load_image/load_image_async function
+# profile_image = load_image(str(ctx.author.avatar_url))
+# profile = Editor(profile_image).resize((150, 150)).circle_image()
+
+poppins = Font().poppins(size=40)
+poppins_small = Font().poppins(size=30)
+
+square = Canvas((500, 500), "#2C2F33")
+square = Editor(canvas=square)
+square.rotate(30, expand=True)
+
+background.paste(square.image, (600, -250))
+background.paste(profile.image, (30, 30))
+
+background.rectangle((30, 220), width=650, height=40, fill="#494b4f", radius=20)
+background.bar((30, 220), max_width=650, height=40, percentage=user_data['percentage'] ,fill="#3db374", radius=20)
+background.text((200, 40), user_data['name'], font=poppins, color="white")
+
+background.rectangle((200, 100), width=350, height=2, fill="#17F3F6")
+background.text((200, 130), f"Level : {user_data['level']} " + f" XP : {user_data['xp']} / {user_data['next_level_xp']}", font=poppins_small, color="white")
+
+
+background.show()
