c065814a1a3e33459cdc0727d2286b6f5bbcd8d1
Shahriyar <mdshahriyaralam552@gmail.com>
2021-11-08 18:07:40 +0600

    Smarter setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
+import re
 import pathlib
 from setuptools import setup, find_packages
 
-here = pathlib.Path(__file__).parent.resolve()
+requirements = []
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
-long_description = (here / "README.md").read_text(encoding="utf-8")
+long_description = (pathlib.Path(__file__).parent.resolve() / "README.md").read_text(
+    encoding="utf-8"
+)
+
+version = ""
+with open("nextcord/__init__.py") as f:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
+    ).group(1)
+
+if not version:
+    raise RuntimeError("version is not set")
 
 setup(
     name="easy-pil",
-    version="0.0.8",
+    version=version,
     description="A library to make common tasks of Pillow easy.",  # Optional
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -29,7 +43,7 @@ setup(
         "easy_pil": ["fonts/*/*.ttf"],
     },
     python_requires=">=3.6, <4",
-    install_requires=["Pillow>=8.3.1", "requests>=2.26.0", "aiohttp>=3.7.4"],
+    install_requires=requirements,
     project_urls={
         "Bug Reports": "https://github.com/shahriyardx/easy-pil/issues",
         "Source": "https://github.com/shahriyardx/easy-pil/",
