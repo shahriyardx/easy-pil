6091930267e8bcb972ce21c3839d38bb7659b6e0
Shahriyar <mdshahriyaralam552@gmail.com>
2021-11-08 18:05:34 +0600

    Making static fonts method static

--- easy_pil/font.py
+++ easy_pil/font.py
@@ -2,64 +2,53 @@ import os
 from PIL import ImageFont
 from typing import Literal
 
+fonts_directory = os.path.join(os.path.dirname(__file__), "fonts")
+fonts_path = {
+    "caveat": {
+        "regular": os.path.join(fonts_directory, "caveat", "caveat.ttf"),
+        "bold": os.path.join(fonts_directory, "caveat", "caveat.ttf"),
+        "italic": os.path.join(fonts_directory, "caveat", "caveat.ttf"),
+        "light": os.path.join(fonts_directory, "caveat", "caveat.ttf"),
+    },
+    "montserrat": {
+        "regular": os.path.join(
+            fonts_directory, "montserrat", "montserrat_regular.ttf"
+        ),
+        "bold": os.path.join(fonts_directory, "montserrat", "montserrat_bold.ttf"),
+        "italic": os.path.join(fonts_directory, "montserrat", "montserrat_italic.ttf"),
+        "light": os.path.join(fonts_directory, "montserrat", "montserrat_light.ttf"),
+    },
+    "poppins": {
+        "regular": os.path.join(fonts_directory, "poppins", "poppins_regular.ttf"),
+        "bold": os.path.join(fonts_directory, "poppins", "poppins_bold.ttf"),
+        "italic": os.path.join(fonts_directory, "poppins", "poppins_italic.ttf"),
+        "light": os.path.join(fonts_directory, "poppins", "poppins_light.ttf"),
+    },
+}
+
 
 class Font:
     def __init__(self, path: str = None, size: int = 10, **kwargs) -> None:
         if path:
             self.font = ImageFont.truetype(path, size=size, **kwargs)
 
-        fonts_directory = os.path.join(os.path.dirname(__file__), "fonts")
-
-        self.__fonts_path = {
-            "caveat": {
-                "regular": os.path.join(fonts_directory, "caveat", "caveat.ttf"),
-                "bold": os.path.join(fonts_directory, "caveat", "caveat.ttf"),
-                "italic": os.path.join(fonts_directory, "caveat", "caveat.ttf"),
-                "light": os.path.join(fonts_directory, "caveat", "caveat.ttf"),
-            },
-            "montserrat": {
-                "regular": os.path.join(
-                    fonts_directory, "montserrat", "montserrat_regular.ttf"
-                ),
-                "bold": os.path.join(
-                    fonts_directory, "montserrat", "montserrat_bold.ttf"
-                ),
-                "italic": os.path.join(
-                    fonts_directory, "montserrat", "montserrat_italic.ttf"
-                ),
-                "light": os.path.join(
-                    fonts_directory, "montserrat", "montserrat_light.ttf"
-                ),
-            },
-            "poppins": {
-                "regular": os.path.join(
-                    fonts_directory, "poppins", "poppins_regular.ttf"
-                ),
-                "bold": os.path.join(fonts_directory, "poppins", "poppins_bold.ttf"),
-                "italic": os.path.join(
-                    fonts_directory, "poppins", "poppins_italic.ttf"
-                ),
-                "light": os.path.join(fonts_directory, "poppins", "poppins_light.ttf"),
-            },
-        }
-
+    @staticmethod
     def poppins(
-        self,
         variant: Literal["regular", "bold", "italic", "light"] = "regular",
         size=10,
     ):
-        return ImageFont.truetype(self.__fonts_path["poppins"][variant], size=size)
+        return ImageFont.truetype(fonts_path["poppins"][variant], size=size)
 
+    @staticmethod
     def caveat(
-        self,
         variant: Literal["regular", "bold", "italic", "light"] = "regular",
         size=10,
     ):
-        return ImageFont.truetype(self.__fonts_path["caveat"][variant], size=size)
+        return ImageFont.truetype(fonts_path["caveat"][variant], size=size)
 
+    @staticmethod
     def montserrat(
-        self,
         variant: Literal["regular", "bold", "italic", "light"] = "regular",
         size=10,
     ):
-        return ImageFont.truetype(self.__fonts_path["montserrat"][variant], size=size)
+        return ImageFont.truetype(fonts_path["montserrat"][variant], size=size)
