fc6284967edd3c6bf0ddbe733d201f915c88bde8
Shahriyar <mdshahriyaralam552@gmail.com>
2021-12-10 03:31:42 +0600

    Made canvas compitable with width and height

--- easy_pil/canvas.py
+++ easy_pil/canvas.py
@@ -6,9 +6,17 @@ from PIL import Image
 class Canvas:
     def __init__(
         self,
-        size: Tuple[float, float],
+        size: Tuple[float, float]=None,
+        width: float = None,
+        height: float = None,
         color: Union[Tuple[int, int, int], str, int] = None,
     ) -> None:
+        if not size and not width and not height:
+            raise ValueError("size, width, and height cannot all be None")
+        
+        if width and height:
+            size = (width, height)
+        
         self.size = size
         self.color = color
 
