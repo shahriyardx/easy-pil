2157d70b8308849b9eecb5be1e61060e8e4c9202
Joseph R. Fox-Rabinovitz <jfoxrabinovitz@gmail.com>
2021-12-08 23:54:05 -0500

    Code cleanup and version parsing modification

--- easy_pil/__init__.py
+++ easy_pil/__init__.py
@@ -1,11 +1,7 @@
-from collections import namedtuple
 from .canvas import Canvas
 from .editor import Editor
 from .font import Font
 from .text import Text
 from .utils import run_in_executor, load_image, load_image_async
+from ._version import __version__, version_info
 
-__version__ = "0.1.1"
-VersionInfo = namedtuple("VersionInfo", "major minor macro release")
-
-version_info = VersionInfo(0, 1, 1, "stable")
