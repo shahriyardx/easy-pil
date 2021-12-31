d6022497b110d4d32cfab6d2e832ef5140af3371
Shahriyar <mdshahriyaralam552@gmail.com>
2021-12-31 23:55:54 +0600

    Reformat conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -51,17 +51,16 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # html_theme = "sphinx_rtd_theme"
 html_theme = "furo"
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
 html_theme_options = {
     "sidebar_hide_name": True,
     "light_logo": "logo.png",
     "dark_logo": "logo.png",
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
 
 # Napoleon settings
 napoleon_numpy_docstring = True
