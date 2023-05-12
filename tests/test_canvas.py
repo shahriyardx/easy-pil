3363a9756c72a5ae3f13ec7a9d568087caa57916
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2023-05-12 20:50:19 +0600

    added another test for canvas with width and height

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -7,7 +7,9 @@ class TestCanvas(unittest.TestCase):
     def test_canvas(self):
         """Tests canvas"""
         canvas = Canvas((100, 100), color="black")
+        canvas2 = Canvas(width=100, height=100, color="black")
         self.assertEqual(canvas.size, (100, 100))
+        self.assertEqual(canvas2.size, (100, 100))
 
 
 if __name__ == "__main__":
